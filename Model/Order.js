c24a80cf8c9003fe313db6e34d38e7cfff976ded
eunbin55 <eunbin620@naver.com>
2025-07-30 18:03:56 +0900

    add Schema

---
+++ Model/Order.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const User = require("./User");
+const Product = require("./Product");
+const Schema = mongoose.Schema;
+const orderSchema = Schema(
+  {
+    userId: { type: mongoose.ObjectId, ref: User },
+    items: [
+      {
+        productId: { type: mongoose.ObjectId, ref: Product },
+        qty: { type: Number, default: 1 },
+        size: { type: String, required: true },
+        price: { type: Number, required: true },
+      },
+    ],
+    // 놓친것..
+    shipTo: { type: Object, required: true },
+    contact: { type: Object, required: true },
+    status: { type: String, default: "preparing" },
+    orderNum: { type: String },
+  },
+  { timestamps: true }
+);
+
+OrderSchema.methods.toJSON = function () {
+  const obj = this._doc;
+  delete obj.__v;
+  delete obj.updatedAt;
+  delete obj.createdAt;
+
+  return obj;
+};
+
+const Order = mongoose.model("Order", orderSchema);
+module.exports = Order;
---
+++ Model/User.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const userSchema = Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    name: { type: String, required: true },
+    // customer | admin
+    level: { type: String, default: "customer" },
+  },
+  { timestamps: true }
+);
+// UserSchema의 데이터를 JSON으로 만들기 전에 delete 속성들을 빼고 obj로 리턴하겠다.
+UserSchema.methods.toJSON = function () {
+  const obj = this._doc;
+  delete obj.password;
+  delete obj.__v;
+  delete obj.updatedAt;
+  delete obj.createdAt;
+
+  return obj;
+};
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
