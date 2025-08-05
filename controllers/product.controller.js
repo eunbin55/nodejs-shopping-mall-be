f4b1dd126e1f71430137afaca68f5126ae9f0edc
eunbin55 <eunbin620@naver.com>
2025-08-05 15:20:09 +0900

    getProducts

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -15,6 +15,9 @@ productController.createProduct = async (req, res) => {
       stock,
       status,
     } = req.body;
+    if (!image) throw new Error("이미지를 추가해주세요.");
+    const findProduct = await Product.findOne({ sku });
+    if (findProduct) throw new Error("이미 등록된 Sku입니다.");
     const product = new Product({
       sku,
       name,
@@ -29,6 +32,15 @@ productController.createProduct = async (req, res) => {
 
     await product.save();
     res.status(200).json({ status: "success", product });
+  } catch (error) {
+    res.status(400).json({ status: "fail", message: error.message });
+  }
+};
+
+productController.getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json({ status: "success", products });
   } catch (error) {
     res.status(400).json({ status: "fail", message: error.message });
   }
--- routes/product.api.js
+++ routes/product.api.js
@@ -3,11 +3,15 @@ const authController = require("../controllers/auth.controller");
 const productController = require("../controllers/product.controller");
 const router = express.Router();
 
+// 상품 생성
 router.post(
   "/",
   authController.authenticate,
   authController.checkAdminPermission,
   productController.createProduct
 );
+
+// 상품 가져오기
+router.get("/", productController.getProducts);
 
 module.exports = router;
