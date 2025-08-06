d420f72ab26b6bcfc15f99221b64520c00b6d00c
eunbin55 <eunbin620@naver.com>
2025-08-06 18:05:45 +0900

    add find option

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,7 +1,7 @@
 const Product = require("../models/Product");
 
 const productController = {};
-
+const PAGE_SIZE = 3;
 productController.createProduct = async (req, res) => {
   try {
     const {
@@ -39,8 +39,26 @@ productController.createProduct = async (req, res) => {
 
 productController.getProducts = async (req, res) => {
   try {
-    const products = await Product.find();
-    res.status(200).json({ status: "success", products });
+    const { page, name } = req.query;
+    const condition = name ? { name: { $regex: name, $options: "i" } } : {};
+    let query = Product.find(condition);
+    let response = { status: "success" };
+    // 동적 response 생성
+    if (page) {
+      // skip(num) => 스킵하고싶은 데이터 숫자(num)를 넘겨서 데이터 스킵
+      // limit(5) => 최대 5개 데이터를 보내겠다
+      query.skip((page - 1) * PAGE_SIZE).limit(PAGE_SIZE);
+      const totalItemNum = await Product.find(
+        condition
+      ).estimatedDocumentCount();
+      const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+      response.totalPageNum = totalPageNum;
+    }
+    // query.exec() => 쿼리 실행
+    const productList = await query.exec();
+    response.data = productList;
+
+    res.status(200).json(response);
   } catch (error) {
     res.status(400).json({ status: "fail", message: error.message });
   }
