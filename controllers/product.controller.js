f0ff23680c0418e98c3786a2f3b56051b1243aad
eunbin55 <eunbin620@naver.com>
2025-08-07 14:57:51 +0900

    add update delete, fix countFn

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -40,7 +40,11 @@ productController.createProduct = async (req, res) => {
 productController.getProducts = async (req, res) => {
   try {
     const { page, name } = req.query;
-    const condition = name ? { name: { $regex: name, $options: "i" } } : {};
+    const condition = {
+      isDeleted: false,
+      ...(name && { name: { $regex: name, $options: "i" } }),
+    };
+
     let query = Product.find(condition);
     let response = { status: "success" };
     // 동적 response 생성
@@ -48,9 +52,7 @@ productController.getProducts = async (req, res) => {
       // skip(num) => 스킵하고싶은 데이터 숫자(num)를 넘겨서 데이터 스킵
       // limit(5) => 최대 5개 데이터를 보내겠다
       query.skip((page - 1) * PAGE_SIZE).limit(PAGE_SIZE);
-      const totalItemNum = await Product.find(
-        condition
-      ).estimatedDocumentCount();
+      const totalItemNum = await Product.find(condition).countDocuments();
       const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
       response.totalPageNum = totalPageNum;
     }
@@ -59,6 +61,46 @@ productController.getProducts = async (req, res) => {
     response.data = productList;
 
     res.status(200).json(response);
+  } catch (error) {
+    res.status(400).json({ status: "fail", message: error.message });
+  }
+};
+
+productController.updateProduct = async (req, res) => {
+  try {
+    const productId = req.params.id;
+    const {
+      sku,
+      name,
+      size,
+      image,
+      price,
+      description,
+      category,
+      stock,
+      status,
+    } = req.body;
+    const product = await Product.findByIdAndUpdate(
+      { _id: productId },
+      { sku, name, size, image, price, description, category, stock, status },
+      { new: true }
+    );
+    if (!product) throw new Error("상품이 존재하지 않습니다.");
+    res.status(200).json({ status: "success", data: product });
+  } catch (error) {
+    res.status(400).json({ status: "fail", message: error.message });
+  }
+};
+
+productController.deleteProduct = async (req, res) => {
+  try {
+    const productId = req.params.id;
+    const product = await Product.findByIdAndUpdate(
+      { _id: productId },
+      { isDeleted: true }
+    );
+    if (!product) throw new Error("상품이 존재하지 않습니다.");
+    res.status(200).json({ status: "success", data: product });
   } catch (error) {
     res.status(400).json({ status: "fail", message: error.message });
   }
--- routes/product.api.js
+++ routes/product.api.js
@@ -14,4 +14,20 @@ router.post(
 // 상품 가져오기
 router.get("/", productController.getProducts);
 
+// 상품 수정하기
+router.put(
+  "/:id",
+  authController.authenticate,
+  authController.checkAdminPermission,
+  productController.updateProduct
+);
+
+// 상품 삭제하기
+router.delete(
+  "/:id",
+  authController.authenticate,
+  authController.checkAdminPermission,
+  productController.deleteProduct
+);
+
 module.exports = router;
